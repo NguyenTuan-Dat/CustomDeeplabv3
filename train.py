1e53ff0cc07328341285fda6f808b20c3cf39be1
Đạt Nguyễn <ntdat@Dats-MacBook-Pro.local>
2021-03-03 03:53:28 +0700

    add l2 loss

--- train.py
+++ train.py
@@ -92,7 +92,7 @@ def train(network_backbone, pre_trained_model=None, trainset_filename='/content/
             num_pixels_union_total += num_pixels_union
             num_pixels_intersection_total += num_pixels_intersection
 
-            validation_single_demo(image=image, label=np.squeeze(label, axis=-1), prediction=prediction, demo_dir=os.path.join("/content/CustomDeeplabv3/data/demos/deeplab/resnet_101_voc2012/", 'validation_demo'), val_no=str(time.time()))
+            validation_single_demo(image=image, label=np.squeeze(label, axis=-1), prediction=prediction, demo_dir=os.path.join("/content/CustomDeeplabv3/data/demos/deeplab/resnet_101_voc2012/", 'validation_demo'), val_no=str(i))
 
         mean_IOU = mean_intersection_over_union(num_pixels_union=num_pixels_union_total, num_pixels_intersection=num_pixels_intersection_total)
 
@@ -128,7 +128,7 @@ def train(network_backbone, pre_trained_model=None, trainset_filename='/content/
             num_pixels_union_total += num_pixels_union
             num_pixels_intersection_total += num_pixels_intersection
 
-            validation_demo(images=images, labels=np.squeeze(labels, axis=-1), predictions=predictions, demo_dir=os.path.join("/content/CustomDeeplabv3/data/demos/deeplab/resnet_101_voc2012/", 'training_demo'), batch_no=_)
+            validation_demo(images=images, labels=np.squeeze(labels, axis=-1), predictions=predictions, demo_dir=os.path.join("/content/CustomDeeplabv3/data/demos/deeplab/resnet_101_voc2012/", 'training_demo'), batch_no=i)
         train_iterator.shuffle_dataset()
 
         # print('Training using SBD...')
